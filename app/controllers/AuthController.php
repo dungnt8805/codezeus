c0f82c818b9a6c033dc9aafec1a3b1808e8bad04
Jesse Boyer <aniyishay@gmail.com>
2014-03-25 21:27:15 -0400

    fix

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-class AuthController extends BaseController
+class AuthController extends ControllerBase
 {
 
     public function githubAction()
--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -2,7 +2,7 @@
 
 use Phalcon\Mvc\Controller;
 
-class BaseController extends Controller
+class ControllerBase extends Controller
 {
 
 }
