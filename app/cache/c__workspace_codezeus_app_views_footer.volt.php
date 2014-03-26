30064d83a0a21a522067c011568dd6cb169e8aee
Jesse Boyer <aniyishay@gmail.com>
2014-03-26 00:45:07 +0000

    Update Gitignore

--- app/cache/c__workspace_codezeus_app_views_footer.volt.php
+++
@@ -1,9 +0,0 @@
-<div class="ui inverted red page grid segment">
-<div class="ten wide column">
-    <!-- <div class="ui three column stackable grid">
-        <div class="column"></div>
-        <div class="column"></div>
-        <div class="column"></div>
-  </div> -->
-</div>
-</div>
--- app/cache/c__workspace_codezeus_app_views_partial_header.volt.php
+++
@@ -1,8 +0,0 @@
-<div class="ui page grid overview segment">
-<div class="ui page grid">
-    <div class="ui two wide column"></div>
-    <div class="twelve wide column">
-        <img src="/img/logo.jpg" alt="Code Zeus" class="ui  image" />
-    </div>
-</div>
-</div>
