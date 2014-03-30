2737ecf70d8b0b2c7daddffe4a99a2df7db19539
Jesse Boyer <aniyishay@gmail.com>
2014-03-29 22:01:24 -0400

    wtf man

--- public/index.php
+++ public/index.php
@@ -24,20 +24,15 @@ try {
     // --------------------------------------------------------------------
     // Load Live Files
     // --------------------------------------------------------------------
-    $local_config_file = "../app/config/local-config.php";
     $live_config_file = "../app/config/live-config.php";
     $live_api_file = "../app/config/live-api.php";
 
-    if (file_exists($local_config_file)) {
-        require_once $local_config_file;
-    }
-
     if (file_exists($live_config_file)) {
-        require_once $live_config_file;
+        $config = require_once $live_config_file;
     }
 
     if (file_exists($live_api_file)) {
-        require_once $live_api_file;
+        $api = require_once $live_api_file;
     }
 
     // --------------------------------------------------------------------
